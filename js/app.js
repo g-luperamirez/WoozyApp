74e48b08f793398c57a938c8d32f83f6087f103b
Guadalupe Ramirez <guadaluperamirez@Guadalupes-MacBook-Pro.local>
2019-10-16 13:53:53 -0400

    project 1 update - data working in console. Need to gonnect drink details URL.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,36 @@
 $(() => {
-  //SHOW MY MY DATA
-  const response = apiData => {
-    console.log("API send", apiData);
+  //SHOW ME THE DATA
+  const unpackMyAPI = apiData => {
+    // console.log("API sent", apiData.drinks);
 
-    // for(let i = 0; i < apiData.length; i++) {
-    const DrinkName = $("<h1>")
-      .text(apiData[1].strDrink)
-      .appendTo($(".showMeTheData"));
-    console.log(DrinkName);
-    // }
+    for (let i = 0; i < apiData.drinks.length; i++) {
+      //DRINK NAME
+      const drinkName = $("<h2>")
+        .text(apiData.drinks[i].strDrink)
+        .appendTo(".showMeTheData");
+      console.log(drinkName);
+
+      //DRINK ID (NO SHOW) - USE TO PULL INGRIDIENTS
+      const drinkId = apiData.drinks[i].idDrink;
+      console.log(drinkId);
+
+      //PUSH ID TO FULL DRINK DETAILS URL ----------------------------------
+      const URLPush = {
+        async: true,
+        crossDomain: true,
+        url: `https://the-cocktail-db.p.rapidapi.com/lookup.php?i=${drinkId}`,
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+          "x-rapidapi-key": "439e57812bmsh2e86010b0756704p118e87jsne6da69923959"
+        }
+      };
+      //   $.ajax(URLPush).done(function(unpackDrinkDetails) {
+      console.log(URLPush);
+      //   });
+      //
+      /// END URL PUSH -----------------------------------------------------
+    }
   };
 
   //SEARCH BY ALCOHOL TYPE
@@ -17,21 +39,23 @@ $(() => {
 
     const searchBoxInput = $(".searchBoxInput");
     const searchBoxVal = searchBoxInput.val();
+    const filter = `filter.php?i=${searchBoxVal}`;
     console.log(searchBoxVal);
 
     //AJAX CONNECTION
     const drinksAPI = {
       async: true,
       crossDomain: true,
-      url: `https://the-cocktail-db.p.rapidapi.com/filter.php?i=${searchBoxVal}`,
+      url: `https://the-cocktail-db.p.rapidapi.com/${filter}`,
       method: "GET",
       headers: {
         "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
         "x-rapidapi-key": "439e57812bmsh2e86010b0756704p118e87jsne6da69923959"
       }
     };
-    $.ajax(drinksAPI).done(function(response) {
-      console.log(response);
-    });
+    $.ajax(drinksAPI).then(unpackMyAPI);
   });
 });
+
+//FULL COCKTAIL DETAILS API
+url: "https://the-cocktail-db.p.rapidapi.com/lookup.php?i=11007";
