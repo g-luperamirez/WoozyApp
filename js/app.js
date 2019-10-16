ac7db7be03b993547c681e85244a498d55655133
Guadalupe Ramirez <guadaluperamirez@Guadalupes-MacBook-Pro.local>
2019-10-16 09:53:42 -0400

    creationg of project 1

--- js/app.js
+++ js/app.js
@@ -1,4 +1,37 @@
 $(() => {
-  //DATA LINK
-  //AJAX CONNECTION
+  //SHOW MY MY DATA
+  const response = apiData => {
+    console.log("API send", apiData);
+
+    // for(let i = 0; i < apiData.length; i++) {
+    const DrinkName = $("<h1>")
+      .text(apiData[1].strDrink)
+      .appendTo($(".showMeTheData"));
+    console.log(DrinkName);
+    // }
+  };
+
+  //SEARCH BY ALCOHOL TYPE
+  $(".clickSearch").on("click", event => {
+    event.preventDefault();
+
+    const searchBoxInput = $(".searchBoxInput");
+    const searchBoxVal = searchBoxInput.val();
+    console.log(searchBoxVal);
+
+    //AJAX CONNECTION
+    const drinksAPI = {
+      async: true,
+      crossDomain: true,
+      url: `https://the-cocktail-db.p.rapidapi.com/filter.php?i=${searchBoxVal}`,
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+        "x-rapidapi-key": "439e57812bmsh2e86010b0756704p118e87jsne6da69923959"
+      }
+    };
+    $.ajax(drinksAPI).done(function(response) {
+      console.log(response);
+    });
+  });
 });
